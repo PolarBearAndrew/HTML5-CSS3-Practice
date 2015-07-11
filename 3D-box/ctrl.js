06ff2796fd152601b330acb1c249c9601af05173
Andrew <chenpoanandrew@gmail.com>
2015-07-11 15:33:20 +0800

    add arrow btn

--- 3D-box/ctrl.js
+++ 3D-box/ctrl.js
@@ -1,36 +1,55 @@
+var now = 1;
+
+var sideData = [
+	{ side: 1, right: 3, left: 4, bottom: 6, top: 5 },
+	{ side: 2, right: 3, left: 4, bottom: 5, top: 6 },
+	{ side: 3, right: 2, left: 1, bottom: 6, top: 5 },
+	{ side: 4, right: 1, left: 2, bottom: 6, top: 5 },
+	{ side: 5, right: 3, left: 4, bottom: 1, top: 2 },
+	{ side: 6, right: 3, left: 4, bottom: 2, top: 1 },
+]
+
+
 $(document).ready( function(){
 
+	//自動旋轉animation
 	var autoRolling = function(){
 
 		$('#cube').toggleClass('autoRolling');
 	};
 
+	//切換透明方塊
 	var background = function(){
 
 		$('figure').toggleClass('background');
 	};
 
-	var rollto = function( show ){
 
-		$('#cube').removeClass('autoRolling')
-					.removeClass()
-					.addClass(show);
+	//旋轉至指定面
+	var rollto = function( side ){
+
+		now = side;
+		$('#cube').removeClass()
+					.addClass( 'show' + side );
 	}
 
-	var show1 = function(){ rollto('show1'); };
-	var show2 = function(){ rollto('show2'); };
-	var show3 = function(){ rollto('show3'); };
-	var show4 = function(){ rollto('show4'); };
-	var show5 = function(){ rollto('show5'); };
-	var show6 = function(){ rollto('show6'); };
+	//判定按鈕,啟動旋轉
+	var showClick = function(){
 
+		var side = $(this).attr('data-side');
+		rollto(side);
+	};
+
+	var arrowClick = function(){
+
+		var arrow = $(this).attr('data-arrow');
+		rollto(sideData[ now - 1 ][arrow]);
+	};
+
+	//委派
 	$('body').on('click', '#auto-Rolling', autoRolling );
 	$('body').on('click', '#background', background );
-	$('body').on('click', '#show1', show1 );
-	$('body').on('click', '#show2', show2 );
-	$('body').on('click', '#show3', show3 );
-	$('body').on('click', '#show4', show4 );
-	$('body').on('click', '#show5', show5 );
-	$('body').on('click', '#show6', show6 );
+	$('body').on('click', '.show', showClick );
+	$('body').on('click', '.arrowBtn', arrowClick );
 
 });
