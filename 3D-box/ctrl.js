578543fad226d07a92e3282ee844cb345ca8e0b3
Andrew <chenpoanandrew@gmail.com>
2015-07-11 14:59:46 +0800

    add rolling ctrl

--- 3D-box/ctrl.js
+++ 3D-box/ctrl.js
@@ -1,21 +1,36 @@
 $(document).ready( function(){
 
 	var autoRolling = function(){
+
 		$('#cube').toggleClass('autoRolling');
 	};
 
 	var background = function(){
+
 		$('figure').toggleClass('background');
 	};
 
-	var show1 = function(){
-		$('#cube').removeClass('autoRolling');
-	};
+	var rollto = function( show ){
 
+		$('#cube').removeClass('autoRolling')
+					.removeClass()
+					.addClass(show);
+	}
 
+	var show1 = function(){ rollto('show1'); };
+	var show2 = function(){ rollto('show2'); };
+	var show3 = function(){ rollto('show3'); };
+	var show4 = function(){ rollto('show4'); };
+	var show5 = function(){ rollto('show5'); };
+	var show6 = function(){ rollto('show6'); };
 
 	$('body').on('click', '#auto-Rolling', autoRolling );
 	$('body').on('click', '#background', background );
 	$('body').on('click', '#show1', show1 );
+	$('body').on('click', '#show2', show2 );
+	$('body').on('click', '#show3', show3 );
+	$('body').on('click', '#show4', show4 );
+	$('body').on('click', '#show5', show5 );
+	$('body').on('click', '#show6', show6 );
 
 });
